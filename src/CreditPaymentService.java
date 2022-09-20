372ef8cad30b5e2e5d74ff40212dfb88c37ad36f
Constantin K <kiskiffa@gmail.com>
2022-09-20 08:46:57 +0300

    correct first

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,33 +1,27 @@
 public class CreditPaymentService {
-        // n - срок кредита, кол-во месяцев; s - сумма кредита; p - годовой процент.
-        public double annuity(int n, int s) {
-                double results; // Ежемесячный платеж result = k * s, где k - коэффициент аннуитета, k = a / b;
+    // n - срок кредита, кол-во месяцев; s - сумма кредита; p - годовой процент; iR - Interest Rate (процентная ставка).
+    public double annuity(int loanPeriod, int creditAmount) {
+        double results; // Ежемесячный платеж result = k * s, где k - коэффициент аннуитета, k = a / b;
 
-                double a; // числитель a = i * x, где i = f2 / 100, где f = p / 12; х = y ^ n, где y = 1 + i.
+        double numeratorFormula; // числитель numeratorFormula = iRMonth * exponentiation, где iRMonth = iR / 12 / 100, где iR - годовая ставка по кредиту; exponentiation = iRMonth1 ^ loanPeriod, где iRMonth1 = 1 + iRMonth; loanPeriod — количество месяцев, в течение которых выплачивается кредит.
+        double denominatorFormula; // знаменатель denominatorFormula = exponentiation - 1.
+        double iR = 9.99;
+        double iRMonth;
+        iRMonth = iR / 12 / 100;
 
-                double b; // знаменатель b = x - 1.
-                double p = 9.99;
+        double iRMonth1;
+        iRMonth1 = 1 + iRMonth;
 
-                double f = p / 12;
-                for (int g=1; g <= 1; g++) {
-                        p /= 12;
-                }
-
-                double i;
-                i = f / 100;
-
-
-                double y;
-                y = 1 + i;
-                double x = y;
-                for(int g = 0; g < (n-1); g++) {
-                        y *= x;
-                }
-                a = i * y;
-                b = y - 1;
-                results = (a/b) * s;
-
-                        return results;
-
+        double exponentiation = iRMonth1;
+        for (int g = 0; g < (loanPeriod - 1); g++) {
+            iRMonth1 *= exponentiation;
         }
+
+        numeratorFormula = iRMonth * iRMonth1;
+        denominatorFormula = iRMonth1 - 1;
+        results = (numeratorFormula / denominatorFormula) * creditAmount;
+
+        return results;
+
+    }
 }
